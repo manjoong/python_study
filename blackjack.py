f75da38c90782782ca71c65555697868f7da098f
a <skekf123@naver.com>
2020-02-07 20:52:18 +0900

    add blackjeck

--- blackjack.py
+++ blackjack.py
@@ -1,9 +1,5 @@
-q = list(map(int, input().split()))
-N = q[0]
-M = q[1]
-arr = []
-for i in range(0, N):
-    arr.append(int(input()))
+N, M = list(map(int, input().split()))
+arr = list(map(int, input().split(' ')))
 answer = M
 for i in range(0, N):
     for j in range(0, N):
@@ -14,12 +10,10 @@ for i in range(0, N):
                 if k == i or k == j:
                     k = k+1
                 else:
-                    print("i: " + str(i) + " j: " + str(j) + " k: " + str(k))
                     if M-(arr[i]+arr[j]+arr[k]) < answer and M-(arr[i]+arr[j]+arr[k]) >= 0 :
-                        answer = M-(arr[i]+arr[j]+arr[k])
-                        print(answer)                  
+                        answer = M-(arr[i]+arr[j]+arr[k])                
                     elif M-answer==0:
                         break
 
-print(M+answer)
+print(M-answer)
                  
