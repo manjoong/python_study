e380aa0c4ad00b97009dfe8cdd028b64b278069d
a <skekf123@naver.com>
2020-02-11 23:16:20 +0900

    adding fibo2

--- fibo.py
+++ fibo.py
@@ -4,12 +4,12 @@ n = 6
 f0 = 0
 f1 = 1
 
-
-for i in range(0, ((n/2))):
-    f0 = f1+f0
-    print(f1, f0)
-    f1 = f1+f0
-    print(f1, f0)
+if n > 1:
+    for i in range(0, ((n/2))):
+        f0 = f1+f0
+        print(f1, f0)
+        f1 = f1+f0
+        print(f1, f0)
 
 if n%2 == 0:
     print f0
