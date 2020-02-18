bee7db878310b095b4cb5eabc6c0eb750b6176f9
a <skekf123@naver.com>
2020-02-18 21:47:08 +0900

    add queue

---
+++ queue.py
@@ -0,0 +1,66 @@
+
+# num = int(input())
+
+# command = [" " for _ in range(0, num)]
+
+# for i in range(0, num):
+#     command[i] = input()
+
+num = 15
+command = ["push 1", "push 2", "front", "back", "size", "empty", "pop", "pop", "pop", "size", "empty", "pop", "push 3", "empty", "front"]
+
+arr = []
+
+def push(x):
+    arr.append(x)
+
+def pop():
+    if len(arr) == 0:
+        print(-1)
+    else:
+        print(arr[0])
+        del arr[0]
+
+def size():
+    print(len(arr))
+
+def empty():
+    if len(arr)>0:
+        print(0)
+    else:
+        print(1)
+
+def front():
+    if len(arr) == 0:
+        print(-1)
+    else:
+        print(arr[0])
+
+def back():
+    if len(arr) == 0:
+        print(-1)
+    else:
+        print(arr[len(arr)-1])
+
+
+
+    
+for i in range(0, num): 
+    if "push" in command[i]:
+        push(int(command[i].split(" ")[1]))
+        
+
+    elif "pop" in command[i]:
+        pop()
+
+    elif "size" in command[i]:
+        size()
+
+    elif "empty" in command[i]:
+        empty()
+        
+    elif  "front" in command[i]:
+        front()
+    
+    elif "back" in command[i]:
+        back()
