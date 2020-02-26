cef59e3827970664c7e8154166bc242e7160c9fe
a <skekf123@naver.com>
2020-02-26 22:22:13 +0900

    complate deque

--- deque.py
+++ deque.py
@@ -1,20 +1,19 @@
+# -*- coding: utf-8 -*- 
 
-push_front X: 정수 X를 덱의 앞에 넣는다.
-push_back X: 정수 X를 덱의 뒤에 넣는다.
-pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-size: 덱에 들어있는 정수의 개수를 출력한다.
-empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
-front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-앞-->[1, 2, 3, 4] <-- 뒤
-num = 1
-command = ["back"]
+num = int(input())
+command = [" " for _ in range(0, num)]
+for i in range(0, num):
+    command[i] = input()
+
+# num = 15
+# command = ["push_back 1","push_front 2","front","back","size","empty","pop_front","pop_back","pop_front","size","empty","pop_back","push_front 3","empty","front"]
+
+
 
 arr = []
 
 def push_front(x):
-    arr.insert(x)
+    arr.insert(0, x)
 
 
 def push_back(x):
@@ -31,6 +30,7 @@ def pop_back():
     if len(arr) == 0:
         print(-1)
     else:
+        # print(len(arr))
         print(arr[len(arr)-1])
         del arr[len(arr)-1]
 
@@ -59,12 +59,19 @@ def back():
 
     
 for i in range(0, num): 
-    if "push" in command[i]:
-        push(int(command[i].split(" ")[1]))
+    # print("i는"+ str(i)+"입니다")
+    if "push_front" in command[i]:
+        push_front(int(command[i].split(" ")[1]))
+
+    elif "push_back" in command[i]:
+        push_back(int(command[i].split(" ")[1]))
         
 
-    elif "pop" in command[i]:
-        pop()
+    elif "pop_front" in command[i]:
+        pop_front()
+
+    elif "pop_back" in command[i]:
+        pop_back()
 
     elif "size" in command[i]:
         size()
