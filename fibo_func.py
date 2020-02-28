fab96d4ce0f76b4824adb200d4910e9f8d2bb129
a <skekf123@naver.com>
2020-02-28 21:18:42 +0900

    add 01tile

--- fibo_func.py
+++ fibo_func.py
@@ -1,13 +1,13 @@
 # n = int(input())
-# arr_0 = 0
-# arr_1 = 0
+countzero = 0
+countone = 0
 
 def fibo(x):
     if x == 0:
-        # arr_0 = arr_0 + 1
+        global countzero += 1
         return 0
     elif x == 1:
-        # arr_1 = arr_1 + 1
+        countone += 1
         return 1
     else:
         return fibo(x-1) + fibo(x-2)
