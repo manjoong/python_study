b35970ec056fd30a89d60ff8ef4666285e8a3808
a <skekf123@naver.com>
2020-03-04 23:50:37 +0900

    adding max_tri

--- max_triangle.py
+++ max_triangle.py
@@ -8,9 +8,10 @@ arr_origin[0] = first_num
 
 for i in range(1, n):
     arr_new = list(map(int, input().split(' ')))
-
+    
+    
     for i in range(1, len(arr_new)):
-        arr_origin[i] = max([(arr_new[i]+arr_origin[i-1]),(arr_new[i]+arr_origin[i])])
-
+        arr_origin[i] = max([(arr_new[i]+arr_origin[i-1]),(arr_new[i]+arr_origin[i])])  <--욥분...
+    arr_origin[0] = arr_origin[0] + arr_new[0]
 
 print(max[arr_origin])
