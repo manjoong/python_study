41fbbddf7e18a50e6281e2cf6365682f1a90b9e3
a <skekf123@naver.com>
2020-03-05 00:31:34 +0900

    add max triangle perfactly*******

--- max_triangle.py
+++ max_triangle.py
@@ -4,14 +4,24 @@ first_num = int(input())
 
 arr_origin = [0 for _ in range(0, n)]
 
+
 arr_origin[0] = first_num
 
-for i in range(1, n):
+for j in range(1, n):
     arr_new = list(map(int, input().split(' ')))
     
     
-    for i in range(1, len(arr_new)):
-        arr_origin[i] = max([(arr_new[i]+arr_origin[i-1]),(arr_new[i]+arr_origin[i])])  <--욥분...
+    for i in range(len(arr_new)-1, 0, -1):
+        arr_origin[i] = max([(arr_new[i]+arr_origin[i-1]),(arr_new[i]+arr_origin[i])])
     arr_origin[0] = arr_origin[0] + arr_new[0]
 
-print(max[arr_origin])
+
+    for k in range(0, len(arr_origin)):
+        print(arr_origin[k])
+
+
+
+
+print(max(arr_origin))
+
+
