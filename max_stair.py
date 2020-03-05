3502db028fe8584961f6bb0b16ad47054d5df336
a <skekf123@naver.com>
2020-03-06 00:03:34 +0900

    adding max stair

--- max_stair.py
+++ max_stair.py
@@ -1,2 +1,13 @@
-
 n = int(input())
+
+arr = [0 for _ in range(0, n)]
+
+arr[0] = int(input())
+arr[1] = arr[0]+ int(input())
+
+for i in range(2, n):
+    new = int(input())
+    arr[i] = max([(arr[i-1]+new), (arr[i-2]+new)])
+    
+for i in range(0, n):
+    print(arr[i])
