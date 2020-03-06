36bb83e82150067e118cab0664175b4c3d160215
a <skekf123@naver.com>
2020-03-06 22:58:31 +0900

    fail stair

--- max_stair.py
+++ max_stair.py
@@ -5,9 +5,23 @@ arr = [0 for _ in range(0, n)]
 arr[0] = int(input())
 arr[1] = arr[0]+ int(input())
 
+duplicate=0
+
 for i in range(2, n):
+
     new = int(input())
-    arr[i] = max([(arr[i-1]+new), (arr[i-2]+new)])
+
+    if duplicate != 1:
+        arr[i] = max([(arr[i-1]+new), (arr[i-2]+new)])
+    else:
+        arr[i] = arr[i-2]+new
+        duplicate=0
+
+    if arr[i] == arr[i-1]+new:
+        duplicate=1
+
     
-for i in range(0, n):
-    print(arr[i])
+
+print(arr[n-1])
+
+
