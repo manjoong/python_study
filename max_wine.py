444059743ecdd28ce6003ca9670eb7c1a8ed555d
a <skekf123@naver.com>
2020-03-11 23:39:16 +0900

    add max_wine... fail..

--- max_wine.py
+++ max_wine.py
@@ -8,11 +8,14 @@ for i in range(0, n):
 dp.append(arr[0])
 if n>=2:
     dp.append(arr[0] + arr[1])
+    dp[1] = max(dp[1], dp[0])
 if n >= 3:
     dp.append(max(arr[0], arr[1])+arr[2])
+    dp[2] = max(dp[2], dp[1])
     for i in range(3, n):
 
         dp.append(max((arr[i]+arr[i-1]+dp[i-3]), (arr[i]+dp[i-2])))
+        dp[i] = max(dp[i], dp[i-1])
 
 print(max(dp))
 
