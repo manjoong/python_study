111e50d237242da30940832aec9671fa6fecb151
a <skekf123@naver.com>
2020-03-20 00:05:32 +0900

    complate dp_fibo

--- dp_fibo.py
+++ dp_fibo.py
@@ -1,17 +1,4 @@
 
-# n = int(input())
-
-# arr=[ 0 for _ in range(0, n+1)]
-# arr[0] = 0
-# arr[1] = 1
-
-
-# for i in range(2, n+1):
-#     arr[i] = arr[i-1] + arr[i-2]
-
-
-# for j in range(0, n+1):
-#     print(arr[j])
 
 
 
@@ -21,23 +8,20 @@ arr = [0 for _ in range(0, n)]
 for i in range(0, n):
     arr[i] = int(input())
 
-zero_count = 0
-one_count = 0
 
-def fibo(k):
-    if k==0:
-        global zero_count
-        zero_count = zero_count + 1
-        return(0)
-    elif k==1:
-        global one_count
-        one_count = one_count + 1
-        return(1)
-    else:
-        return(fibo(k-1)+fibo(k-2))
+
+zero_arr = [0 for _ in range(0, max(arr)+1)]
+one_arr = [0 for _ in range(0, max(arr)+1)]
+
+zero_arr[0] = 1
+one_arr[0] = 0
+
+for i in range(1, max(arr)+1):
+    zero_arr[i] = one_arr[i-1]
+    one_arr[i] = one_arr[i-1] + zero_arr[i-1]
+
+
+
 
 for i in range(0, n):
-    fibo(arr[i])
-    print(str(zero_count) + " " + str(one_count))
-    zero_count = 0
-    one_count = 0
+    print(str(zero_arr[arr[i]]) + " " + str(one_arr[arr[i]]))
