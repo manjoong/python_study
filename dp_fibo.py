30f5084ba2337e73044a4087f4697762f4c7365d
a <skekf123@naver.com>
2020-03-19 23:28:10 +0900

    adding dp_fibo

--- dp_fibo.py
+++ dp_fibo.py
@@ -5,11 +5,11 @@ arr=[ 0 for _ in range(0, n+1)]
 arr[0] = 0
 arr[1] = 1
 
-def fibonacci(i):
-    for i in range(2, n+1):
-        arr[i] = arr[i-1] + arr[i-2]
-    return arr[n]
+
+for i in range(2, n+1):
+    arr[i] = arr[i-1] + arr[i-2]
 
 
-print(fibonacci(n))
+for j in range(0, n+1):
+    print(arr[j])
 
