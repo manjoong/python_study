7ff1661cc9fd0bff490cb7002d2162dc960a654d
a <skekf123@naver.com>
2020-03-19 23:43:37 +0900

    adding dp_fibo

--- dp_fibo.py
+++ dp_fibo.py
@@ -1,15 +1,43 @@
 
+# n = int(input())
+
+# arr=[ 0 for _ in range(0, n+1)]
+# arr[0] = 0
+# arr[1] = 1
+
+
+# for i in range(2, n+1):
+#     arr[i] = arr[i-1] + arr[i-2]
+
+
+# for j in range(0, n+1):
+#     print(arr[j])
+
+
+
 n = int(input())
+arr = [0 for _ in range(0, n)]
 
-arr=[ 0 for _ in range(0, n+1)]
-arr[0] = 0
-arr[1] = 1
+for i in range(0, n):
+    arr[i] = int(input())
 
+zero_count = 0
+one_count = 0
 
-for i in range(2, n+1):
-    arr[i] = arr[i-1] + arr[i-2]
+def fibo(k):
+    if k==0:
+        global zero_count
+        zero_count = zero_count + 1
+        return(0)
+    elif k==1:
+        global one_count
+        one_count = one_count + 1
+        return(1)
+    else:
+        return(fibo(k-1)+fibo(k-2))
 
-
-for j in range(0, n+1):
-    print(arr[j])
-
+for i in range(0, n):
+    fibo(arr[i])
+    print(str(zero_count) + " " + str(one_count))
+    zero_count = 0
+    one_count = 0
