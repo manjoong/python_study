1e0a152749343707ecd74cea4a2ff207e8337491
a <skekf123@naver.com>
2020-03-23 22:53:38 +0900

    understand longest arr

--- longest_arr.py
+++ longest_arr.py
@@ -1,13 +1,32 @@
 n = int(input())
-arr = [0 for _ in range(0, n-1)]
+arr = [0 for _ in range(0, n)]
 q = []
 
-for i in range(0, n):
-  q.append(int(input()))
+q = list(map(int, input().split(" ")))
+# for i in range(0, n):
+#   q.append(int(input(" ")))
 
-if 
+arr[0] = q[0]
+if n>1:
+  count = 1
+else:
+  count = 0
+max = 0
 
-arr[0]= 
 
-for i in range(0, n-1):
+for i in range(1, n):
+  arr[i] = q[i]-q[i-1]
+  # print("arr[i] : " + str(arr[i]))
 
+  if max < arr[i] :
+    max = arr[i]
+    # print("max : " +str(max))
+    count += 1
+
+print(count)
+
+#즐가하는 부분의 갯수를 말하는게 아니라
+#수열 안에서 또 다른 수열(부분수열)을 만들껀데, 무조건 증가하는 증가 수열이여야 하며, 그런 증가 수열 중, 가장 길어야함.
+
+# 6
+# 10 20 10 30 20 50
