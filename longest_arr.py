5d2bc623b5402162bfc4187abc7f2b96c7b8c4d2
a <skekf123@naver.com>
2020-03-24 23:42:58 +0900

    longest arr

--- longest_arr.py
+++ longest_arr.py
@@ -3,16 +3,20 @@ dp = [0 for _ in range(0, n)]
 q = []
 
 arr = list(map(int, input().split(" ")))
+dp[0] = 1
+max_value=1
 
 
 for i in range(0, n):
   for j in range(0, i):
-    if 
+    if arr[i]>arr[j]:
+      if dp[j] > max_value:
+        max_value = dp[j]
+        dp[i] = max_value+1
+      
 
-print(count)
+print(max(dp))
 
-#즐가하는 부분의 갯수를 말하는게 아니라
-#수열 안에서 또 다른 수열(부분수열)을 만들껀데, 무조건 증가하는 증가 수열이여야 하며, 그런 증가 수열 중, 가장 길어야함.
 
 # 6
 # 10 20 10 30 20 50
