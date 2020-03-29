2f1231fd78e95697b6a84e252d6a83d6e0eb4aaf
a <skekf123@naver.com>
2020-03-29 23:11:23 +0900

    complate longest arr 11053

--- longest_arr.py
+++ longest_arr.py
@@ -4,18 +4,27 @@ q = []
 
 arr = list(map(int, input().split(" ")))
 dp[0] = 1
-max_value=1
+max_value=0
+temp=0
 
 
 for i in range(0, n):
   for j in range(0, i):
     if arr[i]>arr[j]:
-      if dp[j] > max_value:
-        max_value = dp[j]
+      temp = dp[j]
+      # dp[i] = dp[j]+1
+      if temp > max_value:
+        max_value = temp
         dp[i] = max_value+1
+  
+  if max_value == 0:
+    dp[i] = 1
+  max_value = 0
       
-
+# for i in range(0, n):
+#   print(dp[i])
 print(max(dp))
+# print(max(dp))
 
 
 # 6
