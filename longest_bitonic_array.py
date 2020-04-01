f47f313d47d5b654dd71ac6b10d4c1a40334bf78
a <skekf123@naver.com>
2020-04-01 23:12:00 +0900

    complate bitonic

--- longest_bitonic_array.py
+++ longest_bitonic_array.py
@@ -1,10 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split(" ")))
-arr = reverse_arr[:]
+reverse_arr = list(reversed(arr))
 front_dp = [0 for _ in range(0, n)]
 front_dp[0] = 1
 back_dp = [0 for _ in range(0, n)]
-back_dp[n-1] = 1
+back_dp[0] = 1
+dp =[]
 temp = 0
 max_value = 0
 
@@ -23,9 +24,36 @@ for i in range(0, n):
     max_value = 0
 
 
-for i in range(n, -1, 0):
-    print(front_dp[i])
 
+
+
+# print(front_dp)
+
+
+for i in range(0, n):
+    for j in range(0, i):
+        if reverse_arr[i] > reverse_arr[j]:
+            temp = back_dp[j]+1
+            
+            if temp > max_value:
+                max_value = temp
+        back_dp[i] = max_value
+    if max_value == 0:
+        back_dp[i] = 1
+    temp = 0
+    max_value = 0
+
+back_dp.reverse()
+# print(back_dp)
+
+for i in range(0, n):
+    dp.append(front_dp[i]+back_dp[i])
+
+stand = dp.index(max(dp))
+
+# print(dp)
+# print(stand)
+print(max(dp)-1)
 
 # for i in range(0, n):
 #     for j in range(0, i):
@@ -49,3 +77,4 @@ for i in range(n, -1, 0):
 
 # 1 2 2 1 3 3 4 5 2 1  if basetion is front
 # 1 5 2 1 4 3 3 3 2 1 if basetion is back  -> if sum of front and back is right answer ? 
+
