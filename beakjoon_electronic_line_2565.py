3686ef4878cfc4f03e68f238ddf6b735a3534877
a <skekf123@naver.com>
2020-04-12 23:24:42 +0900

    I use dictionary

--- beakjoon_electronic_line_2565.py
+++ beakjoon_electronic_line_2565.py
@@ -1,11 +1,20 @@
 n = int(input())
 first_pole = [0 for _ in range(0, n)]
 second_pole = [0 for _ in range(0, n)]
+dp = [0 for _ in range(0, n)]
 
 for i in range(0, n):
     string = str(input())
     first_pole[i] = int(string.split(" ")[0])
     second_pole[i] = int(string.split(" ")[1])
+
+first_dic = {}
+for i in first_pole:
+    first_dic[i] = second_pole[first_pole.index(i)]
+
+print(first_dic)
+
+
 
 
 print(first_pole)
