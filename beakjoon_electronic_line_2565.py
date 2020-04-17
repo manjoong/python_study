8f4ee0ba63601dc158a6c295f713afda161021c0
a <skekf123@naver.com>
2020-04-18 02:17:37 +0900

    adding eletrici

--- beakjoon_electronic_line_2565.py
+++ beakjoon_electronic_line_2565.py
@@ -2,6 +2,7 @@ n = int(input())
 first_pole = [0 for _ in range(0, n)]
 second_pole = [0 for _ in range(0, n)]
 dp = [0 for _ in range(0, n)]
+count = 0
 
 for i in range(0, n):
     string = str(input())
@@ -12,20 +13,38 @@ first_dic = {}
 for i in first_pole:
     first_dic[i] = second_pole[first_pole.index(i)]
 
-print(first_dic)
 
-for i in range(0, len(first_dic)):
+first_pole = []
+second_pole = []
+for key, value in first_dic.items():
+    first_pole.append(key)
+    second_pole.append(value)
+    
+temp =0
+max_value = 0
+dp[0] = 1
+for i in range(0, len(second_pole)):
     for j in range(0, i):
-        if first_dic[first_pole[i]]<second_pole[j]:
-            print(first_dic[first_pole[i]])
-            print("pre second = ", second_pole[j])
-            dp[i] = dp[i] + 1
+        if second_pole[i] > second_pole[j]:
+            temp = dp[j] + 1
+            if max_value < temp:
+                max_value = temp
+                dp[i] = max_value
+    if temp == 0:
+        dp[i] = 1
+    temp =0
+    max_value = 0
+
+
+print(len(second_pole)-max(dp))
     
 
+# print(count)
+# print(first_dic)
+# print(first_pole)
+# print(second_pole)
 
-print(first_pole)
-print(second_pole)
-print(dp)
+
 
 # "8"
 # "1 8"
