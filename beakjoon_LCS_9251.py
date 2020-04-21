fdc05a3a90c9cc0d3e7efd733dfa6eb7ff56951a
ManJoong Kim <skekf123@naver.com>
2020-04-21 22:46:12 +0900

    LCS

---
+++ beakjoon_LCS_9251.py
@@ -0,0 +1,19 @@
+
+
+string_1 = input()
+string_2 = input()
+dp = [0 for _ in range(0, max(len(string_1), len(string_2)))]
+
+
+
+print(dp)
+
+dp[0] = 1
+
+# for i in range(0, len(string_1)):
+#     for j in range(0, len(string_2)):
+
+
+
+# "ACAYKP"
+# "CAPCAK"
