636db0d98d1455c2ecca8d049fe50a2c5a875193
ManJoong Kim <skekf123@naver.com>
2020-04-22 23:35:40 +0900

    lcs

--- beakjoon_LCS_9251.py
+++ beakjoon_LCS_9251.py
@@ -2,13 +2,25 @@
 
 string_1 = input()
 string_2 = input()
-dp = [0 for _ in range(0, max(len(string_1), len(string_2)))]
+dp = [0 for _ in range(0, min(len(string_1), len(string_2)))]
+
+temp = 0
+max_value =0
 
 
+
+for i in range(0, len(string_1)):
+    for j in range(0, len(string_2)):
+        temp = dp[j]
+        if max_value < temp:
+            max_value == temp
+        if string_1[i] == string_2[j]:
+            dp[j] = max_value + 1
+    max_value = 0
+    temp = 0
 
 print(dp)
 
-dp[0] = 1
 
 # for i in range(0, len(string_1)):
 #     for j in range(0, len(string_2)):
