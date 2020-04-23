26fcef6e8a857acaf848811c2276617ce23f4cbe
ManJoong Kim <skekf123@naver.com>
2020-04-23 23:58:58 +0900

    adding lcs

--- beakjoon_LCS_9251.py
+++ beakjoon_LCS_9251.py
@@ -2,6 +2,12 @@
 
 string_1 = input()
 string_2 = input()
+temp = ''
+if len(string_2) > len(string_1):
+    temp = string_1
+    string_1 = string_2
+    string_2 = temp
+
 dp = [0 for _ in range(0, min(len(string_1), len(string_2)))]
 
 temp = 0
@@ -12,14 +18,19 @@ max_value =0
 for i in range(0, len(string_1)):
     for j in range(0, len(string_2)):
         temp = dp[j]
+        # print(temp)
         if max_value < temp:
-            max_value == temp
+            # print("i = ", i, "j = ", j, "max = ", max_value)
+            max_value = temp
         if string_1[i] == string_2[j]:
+            # print("j =", j)
             dp[j] = max_value + 1
+            # print("dp[j] =", dp[j])
+            # print(dp)
     max_value = 0
     temp = 0
-
 print(dp)
+print(max(dp))
 
 
 # for i in range(0, len(string_1)):
