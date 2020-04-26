ae89ec4cd9b813c8b6f0b7fbb513b9175b794a6a
ManJoong Kim <skekf123@naver.com>
2020-04-26 20:26:22 +0900

    add blt

--- beakjoon_LCS_9251.py
+++ beakjoon_LCS_9251.py
@@ -2,11 +2,12 @@
 
 string_1 = input()
 string_2 = input()
-temp = ''
+temp_string = ''
 if len(string_2) > len(string_1):
-    temp = string_1
+    temp_string = string_1
     string_1 = string_2
-    string_2 = temp
+    string_2 = temp_string
+
 
 dp = [0 for _ in range(0, min(len(string_1), len(string_2)))]
 
@@ -18,18 +19,19 @@ max_value =0
 for i in range(0, len(string_1)):
     for j in range(0, len(string_2)):
         temp = dp[j]
-        # print(temp)
-        if max_value < temp:
-            # print("i = ", i, "j = ", j, "max = ", max_value)
-            max_value = temp
         if string_1[i] == string_2[j]:
             # print("j =", j)
             dp[j] = max_value + 1
             # print("dp[j] =", dp[j])
-            # print(dp)
+            # print(dp)        
+        # print(temp)
+        if max_value < temp:
+            # print("i = ", i, "j = ", j, "max = ", max_value)
+            max_value = temp
+
     max_value = 0
     temp = 0
-print(dp)
+# print(dp)
 print(max(dp))
 
 
