81b11c747e6c65ff547756683f53aa5a448820ae
ManJoong Kim <skekf123@naver.com>
2020-05-01 23:39:57 +0900

    addign 1912

--- beakjoong_1912_continuous_sum.py
+++ beakjoong_1912_continuous_sum.py
@@ -2,9 +2,20 @@
 n = int(input())
 string = input()
 array = []
+dp = [0 for _ in range(0, n)]
 
 
 for i in range(0, n):
   array.append(string.split(" ")[i])
 
-print(array)
+array = list(map(int, array))
+dp[0] = array[0]
+
+for i in range(1, n):
+  dp[i] = max(array[i], array[i]+dp[i-1])
+
+
+
+
+
+print(max(dp))
