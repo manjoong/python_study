11badf7f39f6eae01082cc9d33e6c29f95a2d98a
ManJoong Kim <skekf123@naver.com>
2020-05-02 23:45:17 +0900

    add...but fail

--- beakjoong_1912_continuous_sum.py
+++ beakjoong_1912_continuous_sum.py
@@ -2,20 +2,26 @@
 n = int(input())
 string = input()
 array = []
-dp = [0 for _ in range(0, n)]
+# dp = [0 for _ in range(0, n)]
 
 
 for i in range(0, n):
   array.append(string.split(" ")[i])
 
 array = list(map(int, array))
-dp[0] = array[0]
+# dp[0] = array[0]
+dp = array[0]
+max_v = array[0]
+temp = array[0]
+
+# for i in range(1, n):
+#   dp[i] = max(array[i], array[i]+dp[i-1])
 
 for i in range(1, n):
-  dp[i] = max(array[i], array[i]+dp[i-1])
+  dp = max(array[i], array[i]+dp)
+  temp = dp
+  if max_v < temp:
+    max_v = temp
+    
 
-
-
-
-
-print(max(dp))
+print(max_v)
