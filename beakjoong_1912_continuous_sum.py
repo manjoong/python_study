be91338db176efbb0c0b30c3dabd4c8fe3ffadf0
ManJoong Kim <skekf123@naver.com>
2020-05-07 23:22:34 +0900

    complate 1912

--- beakjoong_1912_continuous_sum.py
+++ beakjoong_1912_continuous_sum.py
@@ -1,21 +1,15 @@
-
 n = int(input())
-string = input()
 array = []
-# dp = [0 for _ in range(0, n)]
 
 
-for i in range(0, n):
-  array.append(int(string.split(" ")[i]))
+array = list(map(int, input().split(" ")))
 
-# array = list(map(int, array))
-# dp[0] = array[0]
+
 dp = array[0]
 max_v = array[0]
 temp = array[0]
 
-# for i in range(1, n):
-#   dp[i] = max(array[i], array[i]+dp[i-1])
+
 
 for i in range(1, n):
   dp = max(array[i], array[i]+dp)
@@ -25,3 +19,8 @@ for i in range(1, n):
     
 
 print(max_v)
+
+
+
+
+
