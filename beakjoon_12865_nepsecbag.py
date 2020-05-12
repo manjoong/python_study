d5fb5c13aa8e3ab2811899e41c769d259242fec1
ManJoong Kim <skekf123@naver.com>
2020-05-12 23:41:12 +0900

    add nepsec

--- beakjoon_12865_nepsecbag.py
+++ beakjoon_12865_nepsecbag.py
@@ -1,6 +1,3 @@
-
-
-
 need = list(map(int, input().split(" ")))
 count = need[0]
 can_weight = need[1]
@@ -13,15 +10,15 @@ for i in range(0, count):
     weight.append(int(temp[0]))
     values.append(int(temp[1]))
 
-dp = []
-dp = [values[0]]
+dp = [ [0 for _ in range(0, can_weight)] for _ in range(0, count)]
 
-for i in range(1, count):
-    if sum_weight > can_weight:
-         
+# for i in range(1, count):
+#     if sum_weight > can_weight:
+
 
 print(weight)
 print(values)
+print(dp)
 
 
 # "4 7"
