5fbf160edfa1528f8cbb2186d3afffbbc0435d67
ManJoong Kim <skekf123@naver.com>
2020-05-17 21:21:34 +0900

    nepsec..

--- beakjoon_12865_nepsecbag.py
+++ beakjoon_12865_nepsecbag.py
@@ -2,13 +2,13 @@ need = list(map(int, input().split(" ")))
 count = need[0]
 can_weight = need[1]
 
-weight = []
-values = []
+W = []
+V = []
 
 for i in range(0, count):
     temp = input().split(" ")
-    weight.append(int(temp[0]))
-    values.append(int(temp[1]))
+    W.append(int(temp[0]))
+    V.append(int(temp[1]))
 
 dp = [ [0 for _ in range(0, can_weight)] for _ in range(0, count)]
 
@@ -16,8 +16,8 @@ dp = [ [0 for _ in range(0, can_weight)] for _ in range(0, count)]
 #     if sum_weight > can_weight:
 
 
-print(weight)
-print(values)
+print(W)
+print(V)
 print(dp)
 #dp[i][s]는 배낭 크기가 s일 때, i개의 물건을 넣었을 때, 가능한 최대 가치를 의미합니다.
 
